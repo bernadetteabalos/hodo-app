518778c5b7af256054ae7282e7c54ef0bb377658
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-01-16 13:07:04 -0500

    inital commit with base code

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import "../../stylesheets/css/home.css";
 
 const Home = () => {
-  return <div>Hello from HOME</div>;
+  return (
+    <div className="home-page">
+      <h1>Hodo-</h1>
+      <h2>
+        <em>Prefix for 'road', 'path', 'travel'</em>
+      </h2>
+    </div>
+  );
 };
 
 export default Home;
