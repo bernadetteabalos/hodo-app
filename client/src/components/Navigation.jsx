4a5229c40f91d1c0ff8621254630971a96f3195a
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-01-16 11:27:27 -0500

    base code for navigation bar

---
+++ client/src/components/Navigation.jsx
@@ -0,0 +1,37 @@
+//styling
+import { Container, Navbar } from "react-bootstrap";
+import "../stylesheets/css/navigation.css";
+
+const Navigation = () => {
+  return (
+    <>
+      <Navbar className="navbar">
+        <Container>
+          <Navbar.Brand href="/home">
+            <img
+              src="https://media.istockphoto.com/photos/paper-plane-on-red-background-picture-id1358970693?b=1&k=20&m=1358970693&s=170667a&w=0&h=AgDbacTXHFKtBYvnkzpbL5k38J-o08iUx-6j7zPDo6Q="
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="Hodo logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Brand href="/home">
+            <h3>Hodo</h3>
+          </Navbar.Brand>
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Brand href="/login">
+              <h3>Login</h3>
+            </Navbar.Brand>
+            <Navbar.Brand className="vr"></Navbar.Brand>
+            <Navbar.Brand href="/register" className="me-2">
+              <h3>Register</h3>
+            </Navbar.Brand>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Navigation;
