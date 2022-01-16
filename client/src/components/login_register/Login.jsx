d54caf831560cf9ad925cdb6783805c3fe0d1466
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-01-16 11:26:58 -0500

    inital commit for base login page

--- client/src/components/login_register/Login.jsx
+++ client/src/components/login_register/Login.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import { Container, Form, Button } from "react-bootstrap";
+import { useRef } from "react";
+
+// import stylesheet
+import "../../stylesheets/css/login.css";
 
 const Login = () => {
-  return <div>Hello From Login</div>;
+  const emailRef = useRef();
+  const pwRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="login-page">
+      <Container className="login-container m-auto">
+        <h1>Login</h1>
+        <Form onSubmit={handleSubmit} className="w-100">
+          <Form.Group className="mb-3">
+            <h4>
+              <Form.Label>Email: </Form.Label>
+            </h4>
+            <Form.Control
+              size="lg"
+              type="email"
+              ref={emailRef}
+              required
+              placeholder="name@example.com"
+            />
+          </Form.Group>
+          <Form.Group>
+            <h4>
+              <Form.Label>Password: </Form.Label>
+            </h4>
+            <Form.Control size="lg" type="password" ref={pwRef} required />
+          </Form.Group>
+          <Button className="w-100 mt-2" type="submit">
+            <h4>Login</h4>
+          </Button>
+        </Form>
+      </Container>
+    </div>
+  );
 };
 
 export default Login;
