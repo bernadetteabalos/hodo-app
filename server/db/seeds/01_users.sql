115ce53c3a88baeddc22149d350e4f7b6d788891
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-01-17 23:14:53 -0500

    update password to hash test

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,18 +1,18 @@
 INSERT INTO
 users(first_name, last_name, email, password)
 VALUES
-    ('Mario', 'Bros', 'mario@nintendo.com', 'test'),
-    ('Luigi', 'Bros', 'luigi@nintendo.com', 'test'),
+    ('Mario', 'Bros', 'mario@nintendo.com', '$2a$10$x2zwO.lEeqwAg5sKiNFjIOaBft7cld6Ey9Ia3XE75yYRftDvVBSfS'),
+    ('Luigi', 'Bros', 'luigi@nintendo.com', '$2a$10$x2zwO.lEeqwAg5sKiNFjIOaBft7cld6Ey9Ia3XE75yYRftDvVBSfS'),
     (
         'Princess',
         'Peach',
         'peach@nintendo.com',
-        'test'
+        '$2a$10$x2zwO.lEeqwAg5sKiNFjIOaBft7cld6Ey9Ia3XE75yYRftDvVBSfS'
     ),
     (
         'Princess',
         'Daisy',
         'daisy@nintendo.com',
-        'test'
+        '$2a$10$x2zwO.lEeqwAg5sKiNFjIOaBft7cld6Ey9Ia3XE75yYRftDvVBSfS'
     ),
-    ('Donkey', 'Kong', 'donkey@nintendo.com', 'test');
+    ('Donkey', 'Kong', 'donkey@nintendo.com', '$2a$10$x2zwO.lEeqwAg5sKiNFjIOaBft7cld6Ey9Ia3XE75yYRftDvVBSfS');
