c07f1643c72280a6914d34fa84f092dd0b045f67
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-01-17 22:01:11 -0500

    update to display currentUser name and profile photo

--- client/src/components/logged_in/Profile.jsx
+++ client/src/components/logged_in/Profile.jsx
@@ -1,23 +1,22 @@
 import { Button } from "react-bootstrap";
 import "../../stylesheets/css/profile.css";
 
-const Profile = () => {
+const Profile = (props) => {
+  const { currentUser } = props;
+  console.log("this is currentUser", currentUser);
   return (
     <div className="profile-page">
       <div className="profile-container">
         <div className="left-profile">
           <div className="profile-photo">
-            <img
-              src="https://media.istockphoto.com/photos/portrait-of-a-young-adult-asian-woman-in-venice-picture-id1157741177?b=1&k=20&m=1157741177&s=170667a&w=0&h=c91rVcd5iQ9Lwv2OMwnE36vXZ8mQKvVKjGVg9elWoRk="
-              alt="profile-photo"
-            />
+            <img src={currentUser.profile_photo} alt="profile-photo" />
           </div>
           <div className="profile-name">
-            <h4>JANE</h4>
+            <h4>{currentUser.first_name}</h4>
           </div>
         </div>
         <div className="right-profile">
-          <div class="itineraries-container">
+          <div className="itineraries-container">
             <h1>My Itineraries</h1>
             <ul>
               <li>JAPAN 2020</li>
