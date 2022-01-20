a90134b0ea0aaf3e41564add3f4daf9688aa05dc
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-01-19 20:11:26 -0500

    remove out comments and board, setBoard

--- client/src/App.js
+++ client/src/App.js
@@ -12,33 +12,15 @@ import About from "./components/about_us/About";
 import MainStage from "./components/board/MainStage";
 
 // import styles
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 import "./stylesheets/css/App.css";
 
 function App() {
-  const initalBoardState = {
-    id: 1,
-    title: "Japan 2022",
-    content: "stuff",
-  };
   const [currentUser, setCurrentUser] = useState({});
-  // hard coded the currentBoard for now
-  const [currentBoard, setCurrentBoard] = useState(initalBoardState);
-  // const { state, dispatch } = useApplicationData();
-
-  // const userList = state.users.map((user) => (
-  //   <li key={user.id}>
-  //     {user.first_name} {user.last_name} {user.email}
-  //   </li>
-  // ));
 
   return (
     <div className="App">
-      {/* <h1>Hello there from App</h1> */}
-      {/* <ul>{userList}</ul>
-      <h1>END</h1> */}
-
       {/* ******DIFFERNT ROUTES */}
       <Navigation currentUser={currentUser} setCurrentUser={setCurrentUser} />
       <Router>
@@ -52,10 +34,12 @@ function App() {
             path="/register"
             element={<Register setCurrentUser={setCurrentUser} />}
           />
-          {currentUser.id && <Route 
-            path="/profile"
-            element={<Profile currentUser={currentUser} />}
-          />}
+          {currentUser.id && (
+            <Route
+              path="/profile"
+              element={<Profile currentUser={currentUser} />}
+            />
+          )}
           <Route
             path="/board/:id"
             element={<MainStage currentUser={currentUser} />}
