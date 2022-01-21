80c808773cf17aee3528b05145bbbf8b6bfedce6
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-01-20 23:55:36 -0500

    remove comments

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -10,8 +10,6 @@ const Navigation = (props) => {
   const navigate = useNavigate();
   const { currentUser, setCurrentUser, showLogin, setShowLogin } = props;
 
-  console.log("huuuhhhhh currentUser--->", currentUser);
-
   const login = () => {
     navigate("/login");
   };
@@ -63,51 +61,9 @@ const Navigation = (props) => {
               <Button onClick={register}>Register</Button>
             </div>
           )}
-          {!showLogin && (
-            <Button onClick={logout}>Logout</Button>
-            // <div>
-            //   <Link to="/">Logout</Link>
-            // </div>
-          )}
+          {!showLogin && <Button onClick={logout}>Logout</Button>}
         </div>
       </div>
-      {/* <Navbar className="navbar">
-        <Container>
-          <Navbar.Brand href="/">
-            <img
-              src="https://media.istockphoto.com/photos/paper-plane-on-red-background-picture-id1358970693?b=1&k=20&m=1358970693&s=170667a&w=0&h=AgDbacTXHFKtBYvnkzpbL5k38J-o08iUx-6j7zPDo6Q="
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="Hodo logo"
-            />
-          </Navbar.Brand>
-          <Navbar.Brand href="/">
-            <h3>Hodo</h3>
-          </Navbar.Brand>
-          <Navbar.Brand href="/about">
-            <span>About Us</span>
-          </Navbar.Brand>
-          <Navbar.Collapse className="justify-content-end">
-            <Navbar.Brand href="/login">
-              <h3>Login</h3>
-            </Navbar.Brand>
-            <Navbar.Brand className="vr"></Navbar.Brand>
-            <Navbar.Brand href="/register" className="me-2">
-              <h3>Register</h3>
-            </Navbar.Brand>
-            <Navbar.Brand
-              href="/"
-              className="me-2"
-              onClick={() => {
-                setCurrentUser({});
-              }}
-            >
-              <h3>Logout</h3>
-            </Navbar.Brand>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar> */}
     </>
   );
 };
