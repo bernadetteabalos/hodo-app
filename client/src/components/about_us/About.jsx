3fc6fb6a0e279905580a8ef22c8bdc5b25f53868
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-01-22 22:49:18 -0500

    add nav bar to about page

--- client/src/components/about_us/About.jsx
+++ client/src/components/about_us/About.jsx
@@ -20,38 +20,46 @@ const About = (props) => {
 
   console.log("this is my current user on line 13 from about--->", currentUser);
   return (
-    <div className="about-page title">
-      <h1>About Us</h1>
-      <div className="about-people">
-        <div className="person bold pop">
-          <h1>Bernadette Abalos</h1>
+    <>
+      <Navigation
+        currentUser={currentUser}
+        setCurrentUser={setCurrentUser}
+        showLogin={showLogin}
+        setShowLogin={setShowLogin}
+        setIdTitle={setIdTitle}
+      />
+      <div className="about-page title">
+        <h1>About Us</h1>
+        <div className="about-people">
+          <div className="person bold pop">
+            <h1>Bernadette Abalos</h1>
+          </div>
+          <div className="person emphasize popsec">
+            <h1>Stephanie Li</h1>
+          </div>
+          <div className="person outline popthird">
+            <h1>Blesilda Mateo</h1>
+          </div>
         </div>
-        <div className="person emphasize popsec">
-          <h1>Stephanie Li</h1>
-
+        <div className="about-stack">
+          <h1>Our Tech Stack:</h1>
         </div>
-        <div className="person outline popthird">
-          <h1>Blesilda Mateo</h1>
+        <div className="thank-you">
+          <h1>
+            & THANK YOU x{count}
+            <button
+              onClick={() => {
+                setCount(count + 1000);
+              }}
+            >
+              <i className="fas fa-glass-cheers"></i> Cheer!
+            </button>
+            to our instructors and mentors at LHL!
+          </h1>
+          <span>It wouldn't be a LHL React project without a counter :) </span>
         </div>
       </div>
-      <div className="about-stack">
-        <h1>Our Tech Stack:</h1>
-      </div>
-      <div className="thank-you">
-        <h1>
-          & THANK YOU x{count}
-          <button
-            onClick={() => {
-              setCount(count + 1000);
-            }}
-          >
-            <i className="fas fa-glass-cheers"></i> Cheer!
-          </button>
-          to our instructors and mentors at LHL!
-        </h1>
-        <span>It wouldn't be a LHL React project without a counter :) </span>
-      </div>
-    </div>
+    </>
   );
 };
 
