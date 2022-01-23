587accb21626a8d6f09bb2081488a61798ec4ed4
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-01-22 23:36:16 -0500

    add comments

--- client/src/components/logged_in/OneTitle.jsx
+++ client/src/components/logged_in/OneTitle.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+// import from other libraries
 import { useNavigate } from "react-router-dom";
-import { Button } from "react-bootstrap";
-
+// import styling
 import "../../stylesheets/css/onetitle.css";
 
+// this component is imported into the Profile Component
 const OneTitle = (props) => {
   const { titleObj } = props;
   const navigate = useNavigate();
+
+  // activated when specific title is clicked -- navigate to specific board
   const handleTitle = (e) => {
     e.preventDefault();
     navigate(`/board/${titleObj.id}`);
   };
+
   return (
     <div>
       <h3 onClick={handleTitle} className="handleTitle">
