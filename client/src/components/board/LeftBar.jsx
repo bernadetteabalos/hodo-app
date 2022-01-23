14fbb89d362c0c0f27f0bfe2802f4e8f03f42fae
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-01-23 01:25:07 -0500

    add pen color and fix image link

--- client/src/components/board/LeftBar.jsx
+++ client/src/components/board/LeftBar.jsx
@@ -5,24 +5,33 @@ import { useState } from "react";
 // styling
 import "../../stylesheets/css/leftbar.css";
 import "../../stylesheets/css/mainstage.css";
+import { useEffect } from "react";
 
 const LeftBar = (props) => {
   // deconstructing the props
   const {
     fillColor,
     setFillColor,
-    strokeColor,
-    setStrokeColor,
+    borderColor,
+    setBorderColor,
     handleClick,
     url,
     setURL,
     resetUrl,
     tool,
     setTool,
+    strokeColor,
+    setStrokeColor,
   } = props;
 
   const [state, setState] = useState({ showing: false });
+  const [imagePlusButton, setImagePlusButton] = useState("image");
   const { showing } = state;
+
+  const handlePen = (e) => {
+    setStrokeColor(e.target.value);
+    setTool("pen");
+  };
 
   return (
     <div className="leftsidebar">
@@ -39,26 +48,24 @@ const LeftBar = (props) => {
         <div className="icons">
           <input
             type="color"
-            value={strokeColor}
-            onChange={(e) => setStrokeColor(e.target.value)}
+            value={borderColor}
+            onChange={(e) => setBorderColor(e.target.value)}
           />
           <i className="bi bi-pen-fill"></i>
         </div>
-
         <Button
           className="button-icons"
           variant="outline-secondary"
-          onClick={() => handleClick("Rect", fillColor, strokeColor)}
+          onClick={() => handleClick("Rect", fillColor, borderColor)}
         >
           <div className="icons">
             <i className="bi bi-phone-landscape shapes"></i>
           </div>
         </Button>
-
         <Button
           className="button-icons"
           variant="outline-secondary"
-          onClick={() => handleClick("Square", fillColor, strokeColor)}
+          onClick={() => handleClick("Square", fillColor, borderColor)}
         >
           <div className="icons">
             <i className="bi bi-square shapes"></i>
@@ -67,7 +74,7 @@ const LeftBar = (props) => {
         <Button
           className="button-icons"
           variant="outline-secondary"
-          onClick={() => handleClick("Circle", fillColor, strokeColor)}
+          onClick={() => handleClick("Circle", fillColor, borderColor)}
         >
           <div className="icons">
             <i className="bi bi-circle shapes"></i>
@@ -76,33 +83,37 @@ const LeftBar = (props) => {
         <Button
           className="button-icons"
           variant="outline-secondary"
-          onClick={() => handleClick("Star", fillColor, strokeColor)}
+          onClick={() => handleClick("Star", fillColor, borderColor)}
         >
           <div className="icons">
             <i className="bi bi-star shapes"></i>
           </div>
         </Button>
-
         <div className="texttools">
           <Button
             className="button-icons"
             variant="outline-secondary"
-            onClick={() => handleClick("Text", fillColor, strokeColor)}
+            onClick={() => handleClick("Text", fillColor, borderColor)}
           >
             <div className="icons">
               <i className="bi bi-fonts shapes"></i>
             </div>
           </Button>
         </div>
+        //*****
         <form
           autoComplete="off"
           onSubmit={(e) => {
             e.preventDefault();
+            setImagePlusButton("image");
+            if (url) {
+              resetUrl();
+            }
           }}
         >
           <input
             className="fade-in"
-            style={{ display: showing ? "block" : "none" }}
+            style={{ display: imagePlusButton === "plus" ? "block" : "none" }}
             name="url"
             type="text"
             placeholder="Enter URL of Image"
@@ -110,7 +121,38 @@ const LeftBar = (props) => {
             onChange={(e) => setURL(e.target.value)}
           />
         </form>
-        <Button
+        {imagePlusButton === "image" && (
+          <Button
+            display={"none"}
+            className="button-icons"
+            variant="outline-secondary"
+            onClick={() => {
+              setImagePlusButton("plus");
+              // resetUrl(); don't need this line
+            }}
+          >
+            <div className="icons">
+              <i className="bi bi-image shapes"></i>
+            </div>
+          </Button>
+        )}
+        {imagePlusButton === "plus" && (
+          <Button
+            className="button-icons"
+            variant="outline-secondary"
+            onClick={() => {
+              setImagePlusButton("image");
+              if (url) {
+                resetUrl();
+              }
+            }}
+          >
+            <div className="icons">
+              <i className="bi bi-plus-square shapes"></i>
+            </div>
+          </Button>
+        )}
+        {/* <Button
           className="button-icons"
           variant="outline-secondary"
           onClick={() => {
@@ -128,9 +170,13 @@ const LeftBar = (props) => {
               className="bi bi-plus-square shapes"
             ></i>
           </div>
-        </Button>
+        </Button> */}
       </div>
       {/* *****PEN TOOLS DROP DOWN***** */}
+      <div className="icons">
+        <input type="color" value={strokeColor} onChange={handlePen} />
+        <i className="bi bi-pen-fill"></i>
+      </div>
       <select
         value={tool}
         onChange={(e) => {
