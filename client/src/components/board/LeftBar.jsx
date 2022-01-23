6f62c839d9cb2529fe09213da670f978d1ab093d
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-01-23 15:07:24 -0500

    merge conflict

--- client/src/components/board/LeftBar.jsx
+++ client/src/components/board/LeftBar.jsx
@@ -27,10 +27,9 @@ const LeftBar = (props) => {
   const [state, setState] = useState({ showing: false });
   const [imagePlusButton, setImagePlusButton] = useState("image");
   const { showing } = state;
-  const isPenActivated = tool === 'pen';
-  const isEraserActivated = tool === 'eraser';
-  const isSelectActivated = tool === 'select';
-
+  const isPenActivated = tool === "pen";
+  const isEraserActivated = tool === "eraser";
+  const isSelectActivated = tool === "select";
 
   const handlePen = (e) => {
     setStrokeColor(e.target.value);
@@ -155,44 +154,56 @@ const LeftBar = (props) => {
             </div>
           </Button>
         )}
-      {/* SELECT */}
-      <Button
-        className="button-icons"
-        variant="outline-secondary"
-        onClick={() => setTool('select')}
-      >
-        <div className="icons">
-          <i style={{display: isSelectActivated ? 'flex' : 'none'}} className="bi bi-hand-index-thumb-fill shapes"></i>
-          <i style={{display: isSelectActivated ? 'none' : 'flex'}} className="bi bi-hand-index-thumb shapes"></i>
+        {/* SELECT */}
+        <Button
+          className="button-icons"
+          variant="outline-secondary"
+          onClick={() => setTool("select")}
+        >
+          <div className="icons">
+            <i
+              style={{ display: isSelectActivated ? "flex" : "none" }}
+              className="bi bi-hand-index-thumb-fill shapes"
+            ></i>
+            <i
+              style={{ display: isSelectActivated ? "none" : "flex" }}
+              className="bi bi-hand-index-thumb shapes"
+            ></i>
+          </div>
+        </Button>
+        {/* PEN */}
+        <div
+          className="icons"
+          onClick={() => setTool(isPenActivated ? "select" : "pen")}
+        >
+          <input type="color" value={strokeColor} onChange={handlePen} />
+          <i
+            style={{ display: isPenActivated ? "flex" : "none" }}
+            className="bi bi-pen-fill shapes"
+          ></i>
+          <i
+            style={{ display: isPenActivated ? "none" : "flex" }}
+            className="bi bi-pen shapes shapesTwo"
+          ></i>
         </div>
-      </Button>
-      {/* PEN */}
-      <div className="icons" onClick={() =>  setTool(isPenActivated ? 'select' : 'pen')}>
-        <input type="color" value={strokeColor} onChange={handlePen} />
-        <i style={{display: isPenActivated ? 'flex': 'none'}} className="bi bi-pen-fill shapes"></i>
-        <i style={{display: isPenActivated ? 'none': 'flex'}} className="bi bi-pen shapes shapesTwo"></i>
+        {/* Eraser */}
+        <Button
+          className="button-icons"
+          variant="outline-secondary"
+          onClick={() => setTool(isEraserActivated ? "select" : "eraser")}
+        >
+          <div className="icons">
+            <i
+              style={{ display: isEraserActivated ? "flex" : "none" }}
+              className="bi bi-eraser-fill shapes"
+            ></i>
+            <i
+              style={{ display: isEraserActivated ? "none" : "flex" }}
+              className="bi bi-eraser shapes"
+            ></i>
+          </div>
+        </Button>
       </div>
-<<<<<<< HEAD
-      {/* *****PEN TOOLS DROP DOWN***** */}
-      <select
-        value={tool}
-        onChange={(e) => {
-          setTool(e.target.value);
-        }}
-=======
-      {/* Eraser */}
-      <Button
-        className="button-icons"
-        variant="outline-secondary"
-        onClick={() => setTool(isEraserActivated ? 'select' : 'eraser')}
->>>>>>> cfb0f355f0c7a909c4ca55b4200252e7237ee5f8
-      >
-        <div className="icons">
-          <i style={{display: isEraserActivated ? 'flex' : 'none'}} className="bi bi-eraser-fill shapes"></i>
-          <i style={{display: isEraserActivated ? 'none' : 'flex'}} className="bi bi-eraser shapes"></i>
-        </div>
-      </Button>
-        </div>
     </div>
   );
 };
