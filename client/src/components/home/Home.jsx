0c42b5e5e48acadec740c3ea5deba32bd8045b08
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-01-22 20:47:21 -0500

    import img and add style to the background

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -25,24 +25,9 @@ const Home = (props) => {
         setIdTitle={setIdTitle}
       />
       <div className="home-page">
-        <h1>Hodo-</h1>
-        <h2>
-          <em>Prefix for 'road', 'path', 'travel'</em>
-        </h2>
-        <div>
-          <img
-            src="https://media.istockphoto.com/photos/the-word-2022-behind-the-tree-of-empty-asphalt-road-at-golden-sunset-picture-id1300086148?b=1&k=20&m=1300086148&s=170667a&w=0&h=vh75YUEVACLRBF96r0pwFof0HX-ZTjX0PwUr2e94h0s="
-            alt="home-page-photo"
-          />
-        </div>
-        <br></br>
-        <h3>
-          <em>
-            A collaborative whiteboard application that allows for the planning
-            of itineraries! :)
-          </em>
-        </h3>
+        <img src="https://i.imgur.com/A8H3iYm.png" alt="hodo-logoo" />
       </div>
+      <div className="bottom-page"></div>
     </>
   );
 };
