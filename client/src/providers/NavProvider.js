ff6e04e3f03def1b132d3785238fd788f1699aed
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-01-24 20:23:29 -0500

    make comments

--- client/src/providers/NavProvider.js
+++ client/src/providers/NavProvider.js
@@ -7,32 +7,32 @@ export const navContext = createContext();
 const NavProvider = (props) => {
   const [showLogin, setShowLogin] = useState("");
 
-  // to display both the login and register buttons. Called when the user is not logged in and on home or about page
+  // to display both the login and register buttons. Called when the user is not logged in and on "home or about page"
   const loginRegisterShow = () => {
     setShowLogin("login-register");
   };
 
-  // to display the login button only. Called when user is on the register page.
+  // to display the login button only. Called when user is on the "register page."
   const loginShow = () => {
     setShowLogin("login");
   };
 
-  // to display the register button. Called when user is on the login page.
+  // to display the register button. Called when user is on the "login page."
   const registerShow = () => {
     setShowLogin("register");
   };
 
-  // to display the logout button only. Called when the user is logged in and on the profile page.
+  // to display the logout button only. Called when the user is logged in and on the "profile page."
   const logoutShow = () => {
     setShowLogin("logout");
   };
 
-  // to display the profile link and the logout button. Called when the user is logged in and on the about page
+  // to display the profile link and the logout button. Called when the user is logged in and on the "about page"
   const profileLogoutShow = () => {
     setShowLogin("profile-logout");
   };
 
-  // to display back button. Called when user on /boards/:id
+  // to display back button. Called when user on /boards/:id ("board page")
   const backShow = () => {
     setShowLogin("back");
   };
